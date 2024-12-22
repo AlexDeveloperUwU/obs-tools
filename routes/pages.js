0569436a10f79e5807b2985d6c6bc81e60fdf42a
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2024-12-22 21:25:09 +0100

    feat: :sparkles: Added the planToday endpoint with the backend

--- routes/pages.js
+++ routes/pages.js
@@ -1,9 +1,38 @@
 import e from "express";
+import path from "path";
+import { configDotenv } from "dotenv";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const router = e.Router();
+configDotenv({ path: path.resolve(__dirname, "env/.env") });
 
 router.get("/", (req, res) => {
-  res.render("index");
+  res.redirect("https://alexdevuwu.com");
+});
+
+router.get("/planToday", async (req, res) => {
+  const key = req.query.key;
+  if (key === process.env.KEY) {
+    const options = { timeZone: "Europe/Madrid", year: "numeric", month: "2-digit", day: "2-digit" };
+    const today = new Intl.DateTimeFormat("es-ES", options).format(new Date());
+    try {
+      const json = await fetch(`${process.env.URL}/eventDate?date=${today}`).then((res) => res.json());
+      if (json.length === 0) {
+        res.render("planToday", { plan: "inexistente!" });
+      } else {
+        console.log(json[0].description);
+        res.render("planToday", { plan: json[0].description });
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  } else {
+    res.redirect("https://alexdevuwu.com");
+  }
 });
 
 export default router;
--- server.js
+++ server.js
@@ -7,10 +7,16 @@ import { dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+//! Load environment variables
+const result = configDotenv({ path: path.resolve(__dirname, "env/.env") });
+if (result.error) {
+  console.error("Error loading .env file", result.error);
+  process.exit(1);
+}
+
 //! App creation and configuration
 const app = e();
 const PORT = process.env.PORT || 3000;
-configDotenv({ path: path.resolve(__dirname, "env/.env") });
 app.use(e.static(path.join(__dirname, "public")));
 
 // View engine setup
@@ -18,7 +24,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 //! Routes
-import * as pages from "./routes/pages";
+import * as pages from "./routes/pages.js";
 app.use("/", pages.router);
 
 //! Initialize the server
