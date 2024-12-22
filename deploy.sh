43e262a6caceff3318b0f072aa8ec8064c4c0af8
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2024-12-22 21:44:28 +0100

    feat: :hammer: Create deployment scripts and Docker file

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+git pull
+docker stop obs-tools && docker rm obs-tools
+docker images -q obs-tools | xargs docker rmi
+docker build -t obs-tools .
+docker run -d --name obs-tools -p 3003:3000 -v /obs-tools/data:/usr/src/app/data -v /obs-tools/env:/usr/src/app/env obs-tools
